187e220226ec5dd56fe80597da6921e1f2371846
sidj286 <caayoncj@gmail.com>
2025-10-28 07:38:15 +0800

    Updated AlumniManagement.jsx

--- src/components/dashboards/AlumniManagement.jsx
+++ src/components/dashboards/AlumniManagement.jsx
@@ -1,18 +1,9 @@
 @@ .. @@
  import React, { useState } from 'react';
- import { 
-   Search, 
-   Filter, 
-   Plus, 
-   Download, 
-   Upload,
-   MoreHorizontal,
-   Edit,
-   Trash2,
-   Eye
- } from 'lucide-react';
+ import { Search, Filter, Plus, Download, Upload, MoreHorizontal, CreditCard as Edit, Trash2, Eye } from 'lucide-react';
 -import { useAlumniData } from '../../hooks/useAlumniData';
 +import { useAlumniData } from '../../hooks/useAlumniData.js';
  import { AddAlumniModal } from '../modals/AddAlumniModal';
  
  export function AlumniManagement() {
+ }
